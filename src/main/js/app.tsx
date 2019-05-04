d9ea2bdadd4e6b3df720ea90f857c8cbe176ccc1
bgpark82 <bgpark82@gmail.com>
2019-05-05 00:16:03 +0900

    modified client side

--- src/main/js/app.tsx
+++ src/main/js/app.tsx
@@ -6,9 +6,16 @@ export default class App extends Component {
 
 render() {  
   return (  
-    <div>  
-      <h1>hello world</h1>
-      <Post></Post>
+    <div className="container">  
+        <div className="row">
+            <div className="col-sm-3">
+              this is the left side 
+            </div>
+            <div className="col-sm-9">	
+              <h1>this is the right side</h1>
+              <Post></Post>
+            </div>
+        </div>
     </div>  
   )  
 }  
