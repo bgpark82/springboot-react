7b95a97cb3c14b03729f45ec8a9a5da5ed34375b
bgpark82 <bgpark82@gmail.com>
2019-05-06 23:16:21 +0900

    Client side basic framework

--- src/main/js/app.tsx
+++ src/main/js/app.tsx
@@ -9,10 +9,19 @@ render() {
     <div className="container">  
         <div className="row">
             <div className="col-sm-3">
-              this is the left side 
+              사진<br/>
+              이름<br/>
+              자기소개<br/>
+              스택<br/>
+              프로젝트<br/>
+              SNS주소<br/>
+
             </div>
             <div className="col-sm-9">	
-              <h1>this is the right side</h1>
+              <h1>자기소개</h1>
+              <h1>스택</h1>
+              <h1>프로젝트</h1>
+              
               <Post></Post>
             </div>
         </div>
