77e94eca22b3c7aa2600620404e273ffa4b11bc2
bgpark82 <bgpark82@gmail.com>
2019-05-14 23:54:28 +0900

    Add font-awesome

--- src/main/js/Component/Profile.js
+++ src/main/js/Component/Profile.js
@@ -8,6 +8,14 @@ export default function Profile() {
         <img src="./img/tistory.png" alt="" className="nav-profile"/>
         <section>박병길</section>
         <p>bgpark82@gmail.com</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4"><i class="fab fa-git"></i></div>
+            <div className="col-sm-4"><i class="fab fa-linkedin-in"></i></div>
+            <div className="col-sm-4"><i class="far fa-comment"></i></div>
+          </div>
+        </div>
+        
     </div>  
   )
 }
